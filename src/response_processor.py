14a6e7380da09c530d4be9a8af5594da47f3f575
Fabricio <fabriciorateni@gmail.com>
2022-01-09 19:29:26 -0300

    Finish v0.1 - MVP working

--- src/response_processor.py
+++ src/response_processor.py
@@ -1,11 +1,13 @@
+import re
+from youtube_auth import get_authenticated_youtube_api
 from youtube_video_info_fetcher import get_video_info, get_video_info_from_dummy
+from youtube_playlist_manager import add_video_playlist
 
 def _extract_description_rom(response: dict) -> str:
     desc = response['items'][0]['snippet']['description']
     return desc
 
 def _extract_sections_from(description: str) -> list:
-    import re
     # Last section is not matched. Assuming it is "Grand Finale" so it is fine to not include it.
     result = re.findall(r"([0-9][0-9]:[0-5][0-9].+?(?=[0-9][0-9]:[0-5][0-9]))", description, re.DOTALL)
 
@@ -14,8 +16,11 @@ def _extract_sections_from(description: str) -> list:
 
     return result
 
-def _get_section_videos(youtube_links_and_descriptions_raw: list) -> list:
-    
+def _extract_video_id_from_link(link):
+    video_id = re.search(r'(?<=v=)[^&#]+', link).group(0)
+    return video_id
+
+def _get_section_videos(youtube_links_and_descriptions_raw: list) -> list:    
     def is_you_tube_link(element: str) -> bool:
         return element.find('youtube.com') != -1 or element.find('youtu.be') != -1
 
@@ -30,7 +35,8 @@ def _get_section_videos(youtube_links_and_descriptions_raw: list) -> list:
 
             current_element['description'] = description_or_video
         else:
-            current_element['video_links'].append(description_or_video)
+            video_id = _extract_video_id_from_link(description_or_video)            
+            current_element['video_links'].append({ 'link': description_or_video, 'video_id': video_id })
 
     return result
 
@@ -50,8 +56,11 @@ def process(response : dict) -> dict:
 
 ##### TEST CODE - REMOVE
 
-# video_info = get_video_info()
-video_info = get_video_info_from_dummy()
+playlist_id = 'PLidK0Jb0r6CKuC9aUEaY7ZnB2KLHLygXN'
+
+
+youtube_api = get_authenticated_youtube_api()
+video_info = get_video_info(youtube_api)
 
 print('\n' * 5)
 print(video_info)
@@ -65,7 +74,8 @@ for section in sections:
         print('\n\t-----------------------------------------------------\n')
         print('\t' + x.get('description'))
         for y in x.get('video_links'):
-            print('\t' + y)
+            print('\t' + y.get('link'))
+            add_video_playlist(youtube_api, playlist_id, y.get('video_id'))
 ##### TEST CODE - REMOVE    
 
 
--- src/youtube_playlist_manager.py
+++ src/youtube_playlist_manager.py
@@ -1,56 +1,14 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.playlists.update
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
-
-import os
-
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-
-scopes = [
-    "https://www.googleapis.com/auth/youtube",
-    "https://www.googleapis.com/auth/youtube.force-ssl",
-    "https://www.googleapis.com/auth/youtubepartner",
-]
-
-
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes
-    )
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials
-    )
-
+def add_video_playlist(youtube_api, playlistId, videoId):
     body = dict(
         snippet=dict(
-            playlistId="PLidK0Jb0r6CKuC9aUEaY7ZnB2KLHLygXN",
-            resourceId={
-                "kind": "youtube#video",
-                "videoId": "w0YHjQkyma8"},
+            playlistId=playlistId,
+            resourceId={"kind": "youtube#video", "videoId": videoId},
             position=0,
         )
     )
 
-    request = youtube.playlistItems().insert(part="snippet", body=body)
+    request = youtube_api.playlistItems().insert(part="snippet", body=body)
 
     response = request.execute()
 
     print(response)
-
-
-if __name__ == "__main__":
-    main()
